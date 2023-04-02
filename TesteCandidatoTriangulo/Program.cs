405c2122f48e3a0c5c44ba63ab7559131f663144
jonathassodrecastro <jonathas_sl@hotmail.com>
2023-04-01 21:02:44 -0300

    Adicionar arquivos de projeto.

---
+++ TesteCandidatoTriangulo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteCandidatoTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo= new Triangulo();  
+
+            Console.WriteLine("Digite o triângulo no formato [[6],[3,5],[9,7,1],[4,6,8,4]]:");
+            string entrada = Console.ReadLine();
+            entrada = entrada.Replace("[[", "").Replace("]]", ""); // Remove os colchetes externos
+            string[] linhas = entrada.Split(new string[] { "],[" }, StringSplitOptions.None); // Separa as linhas
+            int[][] matriz = new int[linhas.Length][];
+            for (int i = 0; i < linhas.Length; i++)
+            {
+                string[] numeros = linhas[i].Split(',');
+                matriz[i] = new int[numeros.Length];
+                for (int j = 0; j < numeros.Length; j++)
+                {
+                    matriz[i][j] = int.Parse(numeros[j]);
+                }
+            }
+
+            int resultado = triangulo.ResultadoTriangulo(matriz);
+            Console.WriteLine("Resultado: " + resultado);
+        }
+    }
+}
---
+++ TesteCandidatoTriangulo/Triangulo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteCandidatoTriangulo
+{
+    public class Triangulo
+    {
+        /// <summary>
+        ///    6
+        ///   3 5
+        ///  9 7 1
+        /// 4 6 8 4
+        /// Um elemento somente pode ser somado com um dos dois elementos da próxima linha. Como o elemento 5 na Linha 2 pode ser somado com 7 e 1, mas não com o 9.
+        /// Neste triangulo o total máximo é 6 + 5 + 7 + 8 = 26
+        /// 
+        /// Seu código deverá receber uma matriz (multidimensional) como entrada. O triângulo acima seria: [[6],[3,5],[9,7,1],[4,6,8,4]]
+        /// </summary>
+        /// <param name="dadosTriangulo"></param>
+        /// <returns>Retorna o resultado do calculo conforme regra acima</returns>
+        public int ResultadoTriangulo(int[][] dadosTriangulo)
+        {
+            
+
+            // Para cada linha do triângulo, exceto a última
+            for (int i = dadosTriangulo.Length - 2; i >= 0; i--)
+            {
+                // Para cada elemento da linha
+                for (int j = 0; j < dadosTriangulo[i].Length; j++)
+                {
+                    // Calcula o máximo entre a soma do elemento atual com os dois elementos abaixo
+                    dadosTriangulo[i][j] += Math.Max(dadosTriangulo[i + 1][j], dadosTriangulo[i + 1][j + 1]);
+                }
+            }
+
+            
+            // Retorna o valor máximo na primeira posição do triângulo
+            return dadosTriangulo[0][0];
+
+            
+        }
+
+
+    }
+}
+
